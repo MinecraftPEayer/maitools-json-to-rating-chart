b27901c22bb5a6821f8a341ef3aca2daf60b2e0d
MinecraftPEayer <87757022+MinecraftPEayer@users.noreply.github.com>
2025-07-29 11:22:28 +0000

    修復"Missing Suspense boundary with useSearchParams"錯誤

--- src/app/link/page.tsx
+++ src/app/link/page.tsx
@@ -1,50 +1,14 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
-import Image from "next/image";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { Suspense } from "react";
+import LinkPage from "./Link";
 
-const LinkPage = () => {
-    let [userData, setUserData] = useState({
-        id: "",
-        username: "",
-        avatar: "",
-    });
-    let code = useSearchParams().get("code");
-    if (!code) {
-        const AUTH_URL = `https://discord.com/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}&scope=identify`;
-
-        window.location.href = AUTH_URL;
-        return;
-    }
-    useEffect(() => {
-        (async () => {
-            const response = await axios.get(
-                `${process.env.NEXT_PUBLIC_REDIRECT_URI}?code=${code}`,
-            );
-            setUserData(response.data);
-
-            window.opener.postMessage(
-                `userData__${JSON.stringify(response.data)}`,
-                "*",
-            );
-            window.close();
-        })();
-    }, []);
-
+const Page = () => {
     return (
-        <div>
-            <p>ID: {userData.id}</p>
-            <p>Username: {userData.username}</p>
-            <Image
-                src={`https://cdn.discordapp.com/avatars/${userData.id}/${userData.avatar}.png`}
-                alt="avatar"
-                width={128}
-                height={128}
-            />
-        </div>
+        <Suspense>
+            <LinkPage />
+        </Suspense>
     );
 };
 
-export default LinkPage;
+export default Page;

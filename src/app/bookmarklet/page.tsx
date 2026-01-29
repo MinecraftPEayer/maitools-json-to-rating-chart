43b0d1d88b62af1661c7a55915ce9801e964b468
MinecraftPEayer <boingli227@outlook.com>
2026-01-29 13:29:08 +0000

    change description

--- src/app/bookmarklet/page.tsx
+++ src/app/bookmarklet/page.tsx
@@ -1,11 +1,12 @@
-import BookmarkletPage from "./BookmarkletPage"
+import BookmarkletPage from "./BookmarkletPage";
 
 export const metadata = {
-    title: 'maibot-bookmarklet',
+    title: "maibot-bookmarklet",
+    description: "Bookmarklet script for maibot",
 };
 
 const Page = () => {
     return <BookmarkletPage />;
-}
+};
 
 export default Page;
